33435e53f58bcbcfa8ed3b6213e839bc9199cb3b
Svitlana Yudina <osbka31@gmail.com>
2023-02-15 15:07:11 +0200

    userCard was created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import { Assignment } from './components/Assignment';
 import { Header } from './components/Header';
+import { UserCard } from './components/UserCard';
 
 export const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ export const App: React.FC = () => {
       <div className="app__content">
         <Header />
         <Assignment />
+        <UserCard />
       </div>
     </div>
   );
