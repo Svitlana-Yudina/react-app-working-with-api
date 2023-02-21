c651ce9b9aac032d610fb0daf920d009d8523e6a
Svitlana Yudina <osbka31@gmail.com>
2023-02-21 14:33:36 +0200

    text inputs were updated

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,10 +1,40 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 /* eslint-disable no-shadow */
 import React, { useCallback, useEffect, useState } from 'react';
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
 import { getPositions } from '../../api/requests';
 import { PositionResponse } from '../../types/positionTypes';
+import { TextInput } from '../TextInput';
 import './SignUpForm.scss';
 
+type Inputs = {
+  name: string,
+  email: string
+  phone: string
+  role: number,
+  photo: File,
+};
+
 export const SignUpForm: React.FC = () => {
+  const namePattern = new RegExp(/^([A-Za-z]{1}[-0-9A-Za-z]{3,})/);
+  const emailPattern = new RegExp(/^([A-Za-z]{1}[0-9A-Za-z]{3,})@([a-z\i]{3,}\.[a-z\i]{2,}$)/);
+  const phonePattern = new RegExp(/^\+38[\(]\d{3}[\)]\d{3}[\-]\d{2}[\-]\d{2}$/);
+
+  const methods = useForm<Inputs>({
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+      role: 0,
+      photo: undefined,
+    },
+    mode: 'all',
+    criteriaMode: 'all',
+  });
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => console.log(data);
+
   const [positions, setPositions] = useState<PositionResponse>({
     success: true,
     positions: [],
@@ -24,130 +54,85 @@ export const SignUpForm: React.FC = () => {
     loadPositions();
   }, []);
 
-  const handleTextInputChange = useCallback((
-    event: React.ChangeEvent<HTMLInputElement>,
-    inputId: string,
-  ) => {
-    const label = document.getElementById(inputId);
-
-    if (event.target.value !== '') {
-      label?.classList.add('signUpForm__textInputLabel--filled');
-    } else {
-      label?.classList.remove('signUpForm__textInputLabel--filled');
-    }
-  }, []);
-
   return (
     <div className="signUpForm">
       <h2 className="signUpForm__title">
         Working with POST request
       </h2>
 
-      <form
-        action="/"
-        method="post"
-        className="signUpForm__form"
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <label
-          id="dataName"
-          className="signUpForm__textInputLabel"
-          data-placeholder="Your name"
+      <FormProvider {...methods}>
+        <form
+          action="/"
+          method="post"
+          className="signUpForm__form"
+          onSubmit={methods.handleSubmit(onSubmit)}
         >
-          <input
-            autoComplete="off"
-            type="text"
+          <TextInput
+            inputId="nameData"
+            pattern={namePattern}
+            helperText="example / min 4 characters"
+            placeholder="Your Name"
             name="name"
-            className="signUpForm__textData"
-            onChange={(event) => {
-              handleTextInputChange(event, 'dataName');
-            }}
           />
-          <span className="signUpForm__helperText">
-            Example
-          </span>
-        </label>
 
-        <label
-          id="dataEmail"
-          className="signUpForm__textInputLabel"
-          data-placeholder="Email"
-        >
-          <input
-            autoComplete="off"
-            type="email"
+          <TextInput
+            inputId="emailData"
+            pattern={emailPattern}
+            helperText="example@gmail.com"
+            placeholder="Email"
             name="email"
-            className="signUpForm__textData"
-            onChange={(event) => {
-              handleTextInputChange(event, 'dataEmail');
-            }}
           />
-          <span className="signUpForm__helperText">
-            example@gmail.com
-          </span>
-        </label>
 
-        <label
-          id="dataPhone"
-          className="
-          signUpForm__textInputLabel"
-          data-placeholder="Phone"
-        >
-          <input
-            autoComplete="off"
-            type="tel"
+          <TextInput
+            inputId="phoneData"
+            pattern={phonePattern}
+            helperText="+38 (XXX) XXX - XX - XX"
+            placeholder="Phone"
             name="phone"
-            // pattern="^\+38\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}"
-            className="signUpForm__textData"
-            onChange={(event) => {
-              handleTextInputChange(event, 'dataPhone');
-            }}
           />
-          <span className="signUpForm__helperText">
-            {'+38 (XXX) XXX - XX - XX'}
-          </span>
-        </label>
 
-        <fieldset className="signUpForm__radioGroup">
-          <legend className="signUpForm__radioTitle">
-            Select your position
-          </legend>
-          {positions.positions.map(position => (
-            <div className="signUpForm__radioContainer" key={position.id}>
-              <input
-                type="radio"
-                name="role"
-                value={position.id}
-                className="signUpForm__radioData"
-                required
-                id={String(position.id)}
-              />
+          <fieldset className="signUpForm__radioGroup">
+            <legend className="signUpForm__radioTitle">
+              Select your position
+            </legend>
+            {positions.positions.map(position => (
+              <div className="signUpForm__radioContainer" key={position.id}>
+                <input
+                  type="radio"
+                  {...methods.register('role', { required: true })}
+                  value={position.id}
+                  className="signUpForm__radioData"
+                  required
+                  id={String(position.id)}
+                />
 
-              <label
-                htmlFor={String(position.id)}
-                className="signUpForm__radioLable"
-              >
-                {position.name}
-              </label>
-            </div>
-          ))}
-        </fieldset>
+                <label
+                  htmlFor={String(position.id)}
+                  className="signUpForm__radioLable"
+                >
+                  {position.name}
+                </label>
+              </div>
+            ))}
+          </fieldset>
 
-        <label className="signUpForm__fileInputLabel">
-          <div className="fileName">Upload your photo</div>
-          <button type="button" className="fileButton">Upload</button>
-          <input type="file" name="photo" className="signUpForm__fileInput"/>
-        </label>
+          <label className="signUpForm__fileInputLabel">
+            <div className="fileName">Upload your photo</div>
+            <button type="button" className="fileButton">Upload</button>
+            <input
+              type="file"
+              {...methods.register('photo')}
+              className="signUpForm__fileInput"/>
+          </label>
 
-        <button
-          type="submit"
-          className="signUpForm__button button button--disabled"
-        >
-          Sign up
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="signUpForm__button button button--disabled"
+          >
+            Sign up
+          </button>
+        </form>
+      </FormProvider>
     </div>
   );
 };
---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,95 @@
+/* eslint-disable no-shadow */
+import React, { useCallback, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import './TextInput.scss';
+
+type Props = {
+  inputId: string,
+  pattern: RegExp,
+  helperText: string,
+  placeholder: string,
+  name: string,
+};
+
+export const TextInput: React.FC<Props> = ({
+  inputId,
+  pattern,
+  helperText,
+  placeholder,
+  name,
+}) => {
+  const [isOnChange, setIsOnChange] = useState(false);
+  const { register, formState: { errors } } = useFormContext();
+
+  const handleTextInputChange = useCallback(() => {
+    const label = document.getElementById(inputId);
+
+    setIsOnChange(true);
+    label?.classList.remove('textInput__textInputLabel--error');
+    label?.classList.add('textInput__textInputLabel--filled');
+  }, []);
+
+  const handleTextInputBlur = useCallback(() => {
+    const label = document.getElementById(inputId);
+
+    setIsOnChange(false);
+    label?.classList.add('textInput__textInputLabel--error');
+  }, []);
+
+  return (
+    <div className="textInput">
+      <label
+        id={inputId}
+        className="textInput__textInputLabel"
+        data-placeholder={placeholder}
+      >
+        <input
+          placeholder={placeholder}
+          type="text"
+          {...register(name, {
+            required: {
+              value: true,
+              message: 'This field is required!',
+            },
+            pattern: {
+              value: pattern,
+              message: `Please Enter ${placeholder} in format '${helperText}'`,
+            },
+            onChange() {
+              handleTextInputChange();
+            },
+            onBlur() {
+              if (errors[name]) {
+                handleTextInputBlur();
+              }
+            },
+          })}
+          className="textInput__textData"
+        />
+        {(!errors[name] || isOnChange)
+        && (
+          <span className="textInput__helperText">
+            {helperText}
+          </span>
+        )}
+
+        <ErrorMessage
+          errors={ errors }
+          name={name}
+          render={({ messages }) => {
+            return (
+              (messages && !isOnChange)
+              && Object.entries(messages).map(([type, message]) => (
+                <span key={type} className="textInput__helperText">
+                  {message}
+                </span>
+              ))
+            );
+          }
+          }
+        />
+      </label>
+    </div>
+  );
+};
